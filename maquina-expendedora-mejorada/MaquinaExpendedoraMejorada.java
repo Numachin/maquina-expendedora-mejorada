54728eacc3f2afd44e06d87fdcb9c84c05d631a8
Numachin <43602959+Numachin@users.noreply.github.com>
2018-11-08 11:59:00 +0100

    Add files via upload

---
+++ maquina-expendedora-mejorada/MaquinaExpendedoraMejorada.java
@@ -0,0 +1,120 @@
+public class MaquinaExpendedoraMejorada {
+    
+    // El precio del billete
+    private int precioBillete;
+    // La cantidad de dinero que lleva metida el cliente actual
+    private int balanceClienteActual;
+    // El total de dinero almacenado en la maquina desde su ultimo vaciado
+    private int totalDineroAcumulado;
+    // El origen del billete
+    private String estacionOrigen;
+    // El destino del billete
+    private String estacionDestino;
+    // Elegir maquina para que de premio o no
+    private boolean tipoMaquina;
+    // Numero billetes vendidos
+    private int numeroVenta;
+    // Numero max billetes
+    private int numeroMax;
+    /**
+     * Crea una maquina expendedora de billetes de tren con el 
+     * precio del billete y el origen y destino dados. Se asume que el precio
+     * del billete que se recibe es mayor que 0.
+     */
+    public MaquinaExpendedoraMejorada(int precioDelBillete, String origen, String destino, boolean premioONo, int numeroMaximo) {
+        precioBillete = precioDelBillete;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = origen;
+        estacionDestino = destino;
+        tipoMaquina = premioONo;
+        numeroMax = numeroMaximo;
+    }
+
+    /**
+     * Devuelve el precio del billete
+     */
+    public int getPrecioBillete() {
+        return precioBillete;
+    }
+
+    /**
+     * Devuelve la cantidad de dinero que el cliente actual lleva introducida
+     */
+    public int getBalanceClienteActual() {
+        return balanceClienteActual;
+    }
+
+    /**
+     * Simula la introduccion de dinero por parte del cliente actual
+     */
+    public void introducirDinero(int cantidadIntroducida) {
+        if (numeroVenta < numeroMax){
+            if(cantidadIntroducida > 0) {
+            balanceClienteActual = balanceClienteActual + cantidadIntroducida;
+            }
+            else {
+            System.out.println(cantidadIntroducida + " no es una cantidad de dinero valida.");
+            }  
+        }
+        else {
+            System.out.println("Número máximo de billetes sacados");
+        }
+    }
+
+    /**
+     * Imprime un billete para el cliente actual
+     */
+    public void imprimirBillete() {
+        if(balanceClienteActual >= precioBillete) {        
+            // Simula la impresion de un billete
+            System.out.println("##################");
+            System.out.println("# Billete de tren:");
+            System.out.println("# De " + estacionOrigen + " a " + estacionDestino);
+            System.out.println("# " + precioBillete + " euros.");
+            System.out.println("##################");
+            System.out.println();         
+    
+            // Actualiza el total de dinero acumulado en la maquina
+            totalDineroAcumulado = totalDineroAcumulado + precioBillete;
+            // Reduce el balance del cliente actual dejandole seguir utilizando la maquina
+            balanceClienteActual = balanceClienteActual - precioBillete;
+            numeroVenta ++;
+            if (tipoMaquina == true){
+                System.out.println ("Su ticket tiene premio");
+            }
+        }
+        else {
+            System.out.println("Necesitas introducir " + (precioBillete - balanceClienteActual) + " euros mas!");
+                    
+        }            
+    }
+    
+    /**
+     * Cancela la operacion de compra del cliente actual y le
+     * devuelve al cliente el dinero que ha introducido hasta el momento
+     */
+    public int cancelarOperacionYDevolverDinero()
+    {
+        int cantidadDeDineroADevolver;
+        cantidadDeDineroADevolver = balanceClienteActual;
+        balanceClienteActual = 0;
+        return cantidadDeDineroADevolver;
+    } 
+
+    public int vaciarDineroDeLaMaquina(){
+      int dineroADevolver = -1;
+       if (balanceClienteActual == 0){
+        dineroADevolver = totalDineroAcumulado;
+        totalDineroAcumulado = 0;
+      }
+      else{
+        System.out.println("No se puede vaciar porque hay una operación en curso.");
+      }
+      return dineroADevolver;
+  }
+}
+
+
+
+
